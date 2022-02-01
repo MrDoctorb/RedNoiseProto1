6ff98ede90198e3a037cd24f9f5a497a6d09e4ed
MrDoctorb <zane@indybrowns.org>
2022-02-01 11:37:57 -0600

    Two ropes

--- Assets/Scripts/RopeGenerator.cs
+++ Assets/Scripts/RopeGenerator.cs
@@ -9,24 +9,26 @@ public class RopeGenerator : MonoBehaviour
     [SerializeField] int halfLength;
     [SerializeField] GameObject ropeJoint;
     [SerializeField] PlayerController plug, port;
-    RopeSpriteBehaviour rsb;
-    SpriteShapeController sprite;   
+    RopeSpriteBehaviour rsbRed, rsbBlue;
+    SpriteShapeController spriteRed, spriteBlue;   
     List<Rigidbody2D> allJoints = new List<Rigidbody2D>();
     void Awake()
     {
-        rsb = transform.parent.GetComponent<RopeSpriteBehaviour>();
-        sprite = GetComponent<SpriteShapeController>();
+        rsbRed = transform.GetChild(0).GetComponent<RopeSpriteBehaviour>();
+        rsbBlue = transform.GetChild(1).GetComponent<RopeSpriteBehaviour>();
+        spriteRed = rsbRed.GetComponent<SpriteShapeController>();
+        spriteBlue = rsbBlue.GetComponent<SpriteShapeController>();
         //Temp while testing
         plug.allCordJoints.Clear();
         port.allCordJoints.Clear();
-        rsb.points.Clear();
+        rsbRed.points.Clear();
+        rsbBlue.points.Clear();
 
         //Make a number of joints for the length we want
         for (int i = 1; i <= halfLength; ++i)
         {
             //Move each joint over slightly so it forms a rope
             GameObject newJoint = Instantiate(ropeJoint, plug.transform.position + new Vector3(.5f * i, 0), Quaternion.identity);
-
 
             //Set the joints
             if (allJoints.Count > 0)
@@ -78,15 +80,30 @@ public class RopeGenerator : MonoBehaviour
         {
             joint.GetComponent<HingeJoint2D>().autoConfigureConnectedAnchor = false;
             joint.transform.SetParent(transform);
-            rsb.points.Add(joint.transform);
         }
 
-        int numPoints = sprite.spline.GetPointCount();
-        for(int i = 0; i < allJoints.Count - numPoints; ++i)
+        for(int i = 0; i <= allJoints.Count/2; ++i)
         {
-            sprite.spline.InsertPointAt(3, Vector2.up * i);
+            rsbRed.points.Add(allJoints[i].transform);
         }
-        numPoints = sprite.spline.GetPointCount();
-        rsb.points.Add(port.transform);
+        for(int i = allJoints.Count/2; i < allJoints.Count; ++i)
+        {
+            rsbBlue.points.Add(allJoints[i].transform);
+        }
+
+        int numPointsRed = spriteRed.spline.GetPointCount();
+        for(int i = 0; i < allJoints.Count/2 - numPointsRed; ++i)
+        {
+            spriteRed.spline.InsertPointAt(3, Vector2.up * i);
+        }
+        numPointsRed = spriteRed.spline.GetPointCount();
+
+        int numPointsBlue = spriteBlue.spline.GetPointCount();
+        for (int i = 0; i < allJoints.Count / 2 - numPointsBlue; ++i)
+        {
+            spriteBlue.spline.InsertPointAt(3, Vector2.up * i);
+        }
+        numPointsBlue = spriteBlue.spline.GetPointCount();
+        rsbBlue.points.Add(port.transform);
     }
 }
