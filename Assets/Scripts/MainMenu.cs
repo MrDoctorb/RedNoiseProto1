259b40c8ab76d9033750d54bd3d6c57bd62d62d4
MrDoctorb <zane@indybrowns.org>
2022-02-06 21:57:30 -0600

    Menuing

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,15 +4,25 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-   /// <summary>
-   /// Controls the play button in the main menu.
-   /// </summary>
-   public void Play()
-   {
+    /// <summary>
+    /// Controls the play button in the main menu.
+    /// </summary>
+    public void Play()
+    {
         SceneManager.LoadScene(1);
-   }
+    }
 
-   
+    public void MenuScreen()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void SandBox()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+
     /// <summary>
     /// Controls the quit button in the main menu.
     /// </summary>
